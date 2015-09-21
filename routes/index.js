e6250f08f0298eda8f934e8b02baaf2bd92ebbf0
kolbeypruitt <kolbeypruitt@gmail.com>
2015-09-21 12:43:52 -0600

    sass initialized

--- routes/index.js
+++ routes/index.js
@@ -2,9 +2,18 @@ var express = require('express');
 var router = express.Router();
 
 
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Subaru WRX Trunk Swapper'  });
+});
+
+router.get('/post_trunk', function(req, res, next) {
+  res.render('post_trunk/index', { title: 'Subaru WRX Trunk Swapper'  });
+});
+
+router.get('/search_trunk', function(req, res, next) {
+  res.render('search_trunk/index', { title: 'Subaru WRX Trunk Swapper'  });
 });
 
 module.exports = router;
