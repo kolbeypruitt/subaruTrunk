a2133f57bda87683ed9109653a4cde6f3d1b1396
kolbeypruitt <kolbeypruitt@gmail.com>
2015-09-24 14:12:24 -0600

    OMG it works

--- public/javascripts/search_trunk.js
+++ public/javascripts/search_trunk.js
@@ -1,179 +1,99 @@
 
 $(document).ready(function(){
+  $("#model").prop('disabled', true);  // find id='model' & create disabled property with value of true.
+  $( "#year" ).change(function() {  // when value of elem with id='year' is changed....
+    $("#model").prop('disabled', false);  // change model property disabled to false.
+  });
 
-$("#model").prop('disabled', true);  // find id='model' & create disabled property with value of true.
-$( "#year" ).change(function() {  // when value of elem with id='year' is changed....
-  $("#model").prop('disabled', false);  // change model property disabled to false.
+  $("#color").prop('disabled', true);  // find id='color' & create disabled property with value of true.
+  $( "#model" ).change(function() {  // when value of elem with id='year' is changed....
+    $("#color").prop('disabled', false);  // change color property disabled to false.
+    console.log($("#model").val())
+    genColors($("#year").val(),$("#model").val())
+  });
 
+  $("#trunkStyle").prop('disabled', true);  // find id='trunkStyle' & create disabled property with value of true.
+  $( "#color" ).change(function() {  // when value of elem with id='color' is changed....
+    $("#trunkStyle").prop('disabled', false);  // change trunkStyle property disabled to false.
+  });
+
+
+  
 });
-$("#color").prop('disabled', true);  // find id='color' & create disabled property with value of true.
-$( "#model" ).change(function() {  // when value of elem with id='year' is changed....
-  $("#color").prop('disabled', false);  // change color property disabled to false.
-  getColors($("#year").val(),$("#model").val())
-});
-
-$("#trunkStyle").prop('disabled', true);  // find id='trunkStyle' & create disabled property with value of true.
-$( "#color" ).change(function() {  // when value of elem with id='color' is changed....
-  $("#trunkStyle").prop('disabled', false);  // change trunkStyle property disabled to false.
-});
-
-
-
-
-
-
-
-});
-
 
 var obj = 
 {
   "2014": {
+    "wrx":{
+      "Crystal Black Silica":"#080808",
+      "Satin White Pearl":"#EEECE9",
+      "Dark Gray Metallic":"#1C1F23",
+      "Plasma Blue Silica":"#0E162E",
+      "Ice Silver Metallic":"#BCC0C4",
+      "Lightning Red":"#A72A36",
+      "World Rally Blue Pearl":"#0E4B82"
+    },
     "sti":{
-      "color": [
-        {
-          "lngColor":"Crystal Black Silica","code": "#080808"
-        },
-        {
-          "lngColor":"Satin White Pearl","code": "#EEECE9"
-        },
-        {
-          "lngColor":"Dark Gray Metallic","code": "#1C1F23"
-        },
-        {
-          "lngColor":"Plasma Blue Silica","code": "#0E162E"
-        },
-        {
-          "lngColor":"Ice Silver Metallic","code": "#BCC0C4"
-        },
-        {
-          "lngColor":"Lightning Red","code": "#A72A36"
-        },
-        {
-          "lngColor":"World Rally Blue Pearl","code": "#0E4B82"
-        }
-      ]
+      "Crystal Black Silica":"#080808",
+      "Satin White Pearl":"#EEECE9",
+      "Dark Gray Metallic":"#1C1F23",
+      "Plasma Blue Silica":"#0E162E",
+      "Ice Silver Metallic":"#BCC0C4",
+      "Lightning Red":"#A72A36",
+      "World Rally Blue Pearl":"#0E4B82"
     }
   },
   "2015": {
     "wrx":{
-      "color": [
-        {
-          "lngColor":"Crystal Black Silica","code": "#080808"
-        },
-        {
-          "lngColor":"Crystal White Pearl","code": "#EEECE9"
-        },
-        {
-          "lngColor":"Dark Gray Metallic","code": "#1C1F23"
-        },
-        {
-          "lngColor":"Lapis Blue Pearl","code": "#2C4C6F"
-        },
-        {
-          "lngColor":"Ice Silver Metallic","code": "#BCC0C4"
-        },
-        {
-          "lngColor":"Lightning Red","code": "#A72A36"
-        },
-        {
-          "lngColor":"World Rally Blue Pearl","code": "#0E4B82"
-        }
-      ]
+      "Crystal Black Silica":"#080808",
+      "Crystal White Pearl":"#EEECE9",
+      "Dark Gray Metallic":"#1C1F23",
+      "Lapis Blue Pearl":"#2C4C6F",
+      "Ice Silver Metallic":"#BCC0C4",
+      "Lightning Red":"#A72A36",
+      "World Rally Blue Pearl":"#0E4B82"
     },
     "sti":{
-      "color": [
-        {
-          "lngColor":"Crystal Black Silica","code": "#080808"
-        },
-        {
-          "lngColor":"Crystal White Pearl","code": "#EEECE9"
-        },
-        {
-          "lngColor":"Dark Gray Metallic","code": "#1C1F23"
-        },
-        {
-          "lngColor":"Lapis Blue Pearl","code": "#2C4C6F"
-        },
-        {
-          "lngColor":"Ice Silver Metallic","code": "#BCC0C4"
-        },
-        {
-          "lngColor":"Lightning Red","code": "#A72A36"
-        },
-        {
-          "lngColor":"World Rally Blue Pearl","code": "#0E4B82"
-        }
-      ]
+      "Crystal Black Silica":"#080808",
+      "Crystal White Pearl":"#EEECE9",
+      "Dark Gray Metallic":"#1C1F23",
+      "Lapis Blue Pearl":"#2C4C6F",
+      "Ice Silver Metallic":"#BCC0C4",
+      "Lightning Red":"#A72A36",
+      "World Rally Blue Pearl":"#0E4B82"
     }
   },
-  "2016": {
+  "2016":{
     "wrx":{
-      "color": [
-        {
-          "lngColor":"Crystal Black Silica","code": "#080808"
-        },
-        {
-          "lngColor":"Crystal White Pearl","code": "#EEECE9"
-        },
-        {
-          "lngColor":"Dark Gray Metallic","code": "#1C1F23"
-        },
-        {
-          "lngColor":"Galaxy Blue Pearl","code": "#182D58"
-        },
-        {
-          "lngColor":"Ice Silver Metallic","code": "#BCC0C4"
-        },
-        {
-          "lngColor":"Lightning Red","code": "#A72A36"
-        },
-        {
-          "lngColor":"World Rally Blue Pearl","code": "#0E4B82"
-        }
-      ]
+      "Crystal Black Silica":"#080808",
+      "Crystal White Pearl":"#EEECE9",
+      "Dark Gray Metallic":"#1C1F23",
+      "Galaxy Blue Pearl":"#182D58",
+      "Ice Silver Metallic":"#BCC0C4",
+      "Lightning Red":"#A72A36",
+      "World Rally Blue Pearl":"#0E4B82"
     },
     "sti":{
-      "color": [
-        {
-          "lngColor":"Crystal Black Silica","code": "#080808"
-        },
-        {
-          "lngColor":"Crystal White Pearl","code": "#EEECE9"
-        },
-        {
-          "lngColor":"Dark Gray Metallic","code": "#1C1F23"
-        },
-        {
-          "lngColor":"Lapis Blue Pearl","code": "#2C4C6F"
-        },
-        {
-          "lngColor":"Ice Silver Metallic","code": "#BCC0C4"
-        },
-        {
-          "lngColor":"Lightning Red","code": "#A72A36"
-        },
-        {
-          "lngColor":"World Rally Blue Pearl","code": "#0E4B82"
-        }
-      ]
+      "Crystal Black Silica":"#080808",
+      "Crystal White Pearl":"#EEECE9",
+      "Dark Gray Metallic":"#1C1F23",
+      "Lapis Blue Pearl":"#2C4C6F",
+      "Ice Silver Metallic":"#BCC0C4",
+      "Lightning Red":"#A72A36",
+      "World Rally Blue Pearl":"#0E4B82"
     }
   }
 }
-// var mywrx = obj[2015].wrx[0]
-// var startYear = obj[0];
-// var endYear = obj[obj.length];
-var model = 'wrx';
-var year = 2015;
-function getColors(year,model) {
-  var out = [];
-  for (var i = 0; i < obj[year][model].color.length; i++) {
-    // out.push(obj[year][model].color[i].lngColor);
-    $("option").append(obj[year][model].color[i].lngColor).attr().attr('name', obj[year][model].color[i].lngColor)
-  }
-  // console.log(out)
-  // console.log(obj[2015].wrx.color[0].lngColor)
+
+function genColors (year) {
+  var selectValues = { "1": "test","2": "test 2" };
+  $.each(obj[2015].wrx, function(key, value) {   
+     $('#color')
+         .append($("<option></option>")
+         .attr("value",key)
+         .text(key)); 
+});
 }
-// getcolors(year,model);
+
 
 
